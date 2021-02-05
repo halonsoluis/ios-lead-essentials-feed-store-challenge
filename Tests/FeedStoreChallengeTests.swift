1cef19f35c155dc718f08e343472a89649128f89
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-02-05 17:03:36 +0100

    Assert That Insert Delivers No Error On Empty Cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -39,14 +39,14 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	
 	func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
 		let sut = makeSUT()
-		
+
 		assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
 	}
 	
 	func test_insert_deliversNoErrorOnEmptyCache() {
-		//		let sut = makeSUT()
-		//
-		//		assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
 	}
 	
 	func test_insert_deliversNoErrorOnNonEmptyCache() {
